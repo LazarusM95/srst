860cfca1f951cbaa2af475c1724592b450e69aaa
Lazar Miljkovic <lazar.teacher.rs@gmail.com>
2026-06-23 00:13:14 +0200

    Updated dependencies, added a roadmap going forward

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+// step 0 create database
+// step 1 confirm connections
+// step 2 language selection
+// step 3 deck selection aka pull decks
+// step 4 get request to anki connect cards one time
+// step 5 get word
+// step 6 translate the word
+// step 7 create example sentence
+// step 8 translate example sentence
+// step 9 input to database
+// step 10 send to anki connect via json
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
