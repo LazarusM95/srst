daf36ae535651b665613ed7e297b98db3ea5d130
Lazar Miljkovic <lazar.teacher.rs@gmail.com>
2026-06-25 16:42:49 +0200

    Added a language selection option

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,35 @@
 // step 8 translate example sentence
 // step 9 input to database
 // step 10 send to anki connect via json
+use std::io;
+
+fn language_select() -> u8 {
+    println!("Please select your target language language:");
+    println!("1)Serbian; 2)German; 3)Chinese; 4)Japanese");
+
+
+    let language = loop {
+        
+    let mut language = String::new();
+
+    io::stdin()
+        .read_line(&mut language)
+        .expect("Wrong input! Failed to read the line.");
+
+     let language: u8 = language.trim().parse().expect("Please type corresponding number");
+     
+        if language >=1 && language <=4 {
+            break language
+        } else{
+            println!("Please enter a valid selection of 1-4");
+        }
+    };
+    
+    language
+}
+
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    language_select();
 }
